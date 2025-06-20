8ce43750c94d26282e0c6b140fc8b68232704a65
RichardDeBrito <richartdebrito2@gmail.com>
2025-06-20 18:23:27 -0300

    testando

--- src/database.ts
+++ src/database.ts
@@ -13,3 +13,5 @@ export async function initDb() {
     const db = await openDb();
     await db.run('CREATE TABLE if not exists Paciente (paciente_id integer primary key AUTOINCREMENT, nome text not null, datanasc text not null, cpf text not null, sexo text not null, telefone text, sus_card text not null)');
 }
+
+console.log("teste")
